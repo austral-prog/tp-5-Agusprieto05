746e3a252680af51d4f2e996be601a0a1fed1bf0
Agusprieto05 <162721778+Agusprieto05@users.noreply.github.com>
2024-04-26 20:40:14 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,5 +1,8 @@
 # Replace the "ANSWER HERE" for your answer
 import math
+
+# Para checkear si anda mira el archivo TEST.py, te escribi como probarlo
+
 def roots(a, b, c):
     discriminante = math.pow(b, 2) - 4 * a * c
     if discriminante < 0:
@@ -10,20 +13,24 @@ def roots(a, b, c):
         return f"({(-b + math.sqrt(discriminante))/2*a}, {(-b - math.sqrt(discriminante))/2*a})"
 
 def value_y(a, b, c, x):
-        return a*x**2 + b*x + c
+        return math.pow(x, 2)*a + b*x + c
 
 
 def to_string(a, b, c):
-    if a == 0:
-        return f"f(x) = {b}x + {c}" 
-    if b == 0:
-        return f"f(x) = {a}x^2 + {c}"
-    if c == 0: 
-        return f"f(x) = {a}x^2 + {b} * X"
+    if a == 0 and b != 0 and c != 0:
+        return f"f(x) = {b} * X + {c}" 
+    
+    if a != 0 and b == 0 and c != 0:
+        return f"f(x) = {a} * X^2 + {c}"
+    
+    if a != 0 and b != 0 and c == 0: 
+        return f"f(x) = {a} * X^2 + {b} * X"
+    
     if a == 0 and b == 0:
         return f"f(x) = {c}"
+    
     else: 
-        return f"f(x) = {a}x^2 + {b}x + {c}"
+        return f"f(x) = {a} * X^2 + {b} * X + {c}"
 
 def derivation(a, b, c):
     if a*2 == 0:
@@ -32,13 +39,6 @@ def derivation(a, b, c):
         return f"f'(x) = {a*2} * X"
     return f"f'(x) = {a*2} * X + {b}"
 
-
-#print(roots(1, -3, 2)) # Retorna: "(2.0, 1.0)"
-#print(roots(1, -2, 1)) # Retorna: "(1.0)"
-#print(roots(1, 2, 3))  # Retorna: "( )"
-
-#print(value_y(1, -3, 2, 0)) # Retorna: 2
-#print(value_y(1, -3, 2, 1)) # Retorna: 0
 #print(value_y(1, -3, 2, -1)) # Retorna: 6
 
 #print(to_string(2, -3, 1)) # Retorna: "f(x) = 2 * X^2 + -3 * X + 1"
